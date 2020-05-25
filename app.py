7d2fdf24556948ad635da3df3798d9198150dfb4
Hanish Arora <61754555+innovation18@users.noreply.github.com>
2020-05-25 22:41:38 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,123 @@
+import os
+from flask import Flask, jsonify, request, abort
+from models import setup_db, Users, Books, Exchange
+from auth.auth import requires_auth, AuthError
+
+
+def create_app():
+    app = Flask(__name__)
+    setup_db(app)
+
+    @app.route("/user", methods=['POST'])
+    def create_user():
+        new_user = request.get_json()
+
+        users = Users(
+            name=new_user['name'],
+            zip_code=new_user['zip_code'],
+            city=new_user['city'],
+            country=new_user['country']
+        )
+
+        users.insert()
+
+        result = {
+            "success": True,
+            "message": 'User ' + new_user['name'] + ' has been added successfully'
+        }
+        return jsonify(result)
+
+    @app.route('/books', methods=['POST', 'GET'])
+    @requires_auth('add:book')
+    def create_book(valid):
+        if valid:
+            if request.method == 'GET':
+                return jsonify({
+                    'books': [Books.details(book) for book in Books.query.all()]
+                })
+
+            else:
+                new_book = request.get_json()
+
+                # user validation to be implemented -- TODO
+
+                book = Books(
+                    title=new_book['title'],
+                    author=new_book['author'],
+                    created_by=new_book['user_id']
+                )
+
+                book.insert()
+
+                result = {
+                    "success": True,
+                    "message": 'book ' + new_book['title'] + ' has been added successfully'
+                }
+                return jsonify(result)
+        else:
+            abort(404)
+
+    @app.route('/request', methods=['POST'])
+    @requires_auth('request-book')
+    def request_book(valid):
+        if valid:
+            try:
+                data = request.get_json()
+            except Exception as e:
+                abort(400, str(e))
+
+            exchange = Exchange(
+                requester_id=data['requester_id'],  # TODO - requester_id later to be picked from db using session user name
+                lender_id=data['lender_id'],
+                book_id=data['book_id'],
+                status='pending for approval'
+            )
+
+            try:
+                exchange.insert()
+            except Exception as e:
+                abort(400, str(e))
+
+            result = {
+                "success": True,
+                "message": 'request submitted successfully'
+            }
+
+            return jsonify(result)
+
+    @app.route('/requests/<int:user_id>', methods=['GET'])
+    def check_for_requests(user_id):
+        # TODO get no. of pending requests for current user id -- which is lender_id
+
+        # count = Exchange.query.filter(Exchange.status != 'approved').filter(Exchange.lender_id == 1).count()
+
+        data = Exchange.query.filter(Exchange.status != 'approved'). \
+            filter(Exchange.lender_id == user_id).all()
+
+        result = {
+            "requests": list(map(Exchange.requests, data))
+        }
+
+        return jsonify(result)
+
+    @app.route('/requests/<int:user_id>', methods=['PATCH'])
+    def handle_requests(user_id):
+        requests = Exchange.query.filter(Exchange.status != 'approved'). \
+            filter(Exchange.lender_id == user_id).one_or_none()
+
+        requests.status = 'approved'
+
+        Exchange.update(requests)
+
+        return jsonify({
+            "success": True,
+            "message": "Your requests are approved"
+        })
+
+    return app
+
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run()
---
+++ models.py
@@ -0,0 +1,127 @@
+import os
+from sqlalchemy import Column, String, Integer, create_engine, ForeignKey, ARRAY, Boolean
+from flask_sqlalchemy import SQLAlchemy
+
+
+database_path = os.environ['DATABASE_URL']
+
+db = SQLAlchemy()
+
+
+def setup_db(app):
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_path
+    db.app = app
+    db.init_app(app)
+    # migrate = Migrate(app, db)
+
+
+class Users(db.Model):
+    __tablename__ = 'users'
+
+    user_id = Column(Integer, primary_key=True)
+    name = Column(String)
+    zip_code = Column(Integer)
+    city = Column(String(120))
+    country = Column(String(100))
+    legit = Column(Boolean)  # TODO legitimacy
+    test = Column(Boolean)  # test migration
+    books = db.relationship('Books', backref='Users', lazy='dynamic')
+    exchange = db.relationship('Exchange', backref='Users', lazy='dynamic')
+
+    def __init__(self, name, city, country, zip_code):
+        self.name = name
+        self.zip_code = zip_code
+        self.city = city
+        self.country = country
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def short(self):
+        return {
+            'id': self.user_id,
+            'name': self.name,
+        }
+
+    def long(self):
+        print(self)
+        return {
+            'id': self.user_id,
+            'name': self.name,
+            'zip_code': self.zip_code,
+            'city': self.city,
+            'country': self.country,
+        }
+
+
+class Books(db.Model):
+    __tablename__ = 'books'
+
+    book_id = Column(Integer, primary_key=True)
+    title = Column(String)
+    author = Column(String(120))
+    available = Column(Boolean)
+    created_by = Column(Integer, ForeignKey('users.user_id'))
+    exchange = db.relationship('Exchange', backref='Books', lazy='dynamic')
+
+    def __init__(self, title, author, created_by):
+        self.title = title
+        self.author = author
+        self.created_by = created_by
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def short(self):
+        return {
+            'id': self.book_id,
+            'title': self.title,
+        }
+
+    def details(self):
+        return {
+            'id': self.book_id,
+            'title': self.title,
+            'author': self.author
+        }
+
+
+class Exchange(db.Model):
+    __tablename__ = 'exchange'
+
+    request_id = Column(Integer, primary_key=True)
+    requester_id = Column(Integer, ForeignKey('users.user_id'))
+    lender_id = Column(Integer)
+    book_id = Column(Integer, ForeignKey('books.book_id'))
+    status = Column(String, nullable=False)
+
+    def __init__(self, requester_id, lender_id, book_id, status):
+        self.lender_id = lender_id
+        self.requester_id = requester_id
+        self.book_id = book_id
+        self.status = status
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def requests(self):
+        return {
+            'requester_id': self.requester_id,
+            'book_id': self.book_id
+        }
